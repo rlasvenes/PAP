3fbc0493084caf41cd9f19cad4d162710f632046
rlasvenes <remi.lasvenes@etu.u-bordeaux.fr>
2020-02-13 18:31:20 +0100

    Modif script

--- easypap-se/test.sh
+++ easypap-se/test.sh
@@ -15,7 +15,7 @@ function compute {
     echo "Launching \"$PROG -s $SIZE -k $KERNEL -g $TILE_SIZE -v $1 -a $NB_SPIRALE -n\""
     filename="${SIZE}_${KERNEL}_${TILE_SIZE}_${NB_SPIRALE}_$1.txt"
     touch $filename
-    $PROG -s $SIZE -k $KERNEL -g $TILE_SIZE -v $1 -a $NB_SPIRALE -n > $filename
+    $PROG -s $SIZE -k $KERNEL -g $TILE_SIZE -v $1 -a $NB_SPIRALE -n > $filename 2>&1 
     runtime=$(cat $filename | cut -d$'\n' -f3)
     echo -e "Time for \"$1\" is $runtime"
 }
