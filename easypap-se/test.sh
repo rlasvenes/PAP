1680ec2d228aa56baca679cd1adfabe0ac0a16f6
rlasvenes <remi.lasvenes@etu.u-bordeaux.fr>
2020-02-13 20:08:39 +0100

    Modif script

--- easypap-se/test.sh
+++ easypap-se/test.sh
@@ -36,13 +36,14 @@ function computeVariants {
     variants_array=("$@")
     for var in "${variants_array[@]}" 
     do
-    f=testGNUPLOT.txt
-    touch $f
-    echo "$var, " >> $f
+    plot_filename="testGNUPLOT.txt"
+    touch $plot_filename
+    echo "$var, " >> $plot_filename
         for (( i=$FROM_TS; i<=$TO_TS; i+=$STEP_TS )); do
             #log "Iterate on $var with tile size of \"$i\""
             value=$(compute $var $i )
-            log "runtime = ${value}"
+            log "runtime = ${value} variant=$var [ts=$i]"
+            echo "$value," >> plot_filename
         done
     done
 }
